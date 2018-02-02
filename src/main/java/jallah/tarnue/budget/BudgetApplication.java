9f7f92d1364c39be4fa3d88a4a29b9b222148357
Tarnue Jallah <jallah02@hotmail.com>
2018-02-01 21:01:08 -0500

    Initial commit

---
+++ src/main/java/jallah/tarnue/budget/BudgetApplication.java
@@ -0,0 +1,11 @@
+package jallah.tarnue.budget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BudgetApplication.class, args);
+    }
+}
